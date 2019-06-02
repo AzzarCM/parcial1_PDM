a73d50e40b01f9b2248a769ac9d0b77427f2d8fb
Cristian Mundo <00086917@uca.edu.sv>
2019-06-02 15:31:23 -0600

    added main, model, fragement xml's

---
+++ app/src/main/java/com/example/basketballscore/fragment/FragmentMatchesList.kt
@@ -0,0 +1,11 @@
+package com.example.basketballscore.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.basketballscore.R
+
+class FragmentMatchesList : Fragment() {
+
+
+}
